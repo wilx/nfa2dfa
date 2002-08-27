1ee2d941d15c2616583a428c19a4d005343b5562
wilx <none>
2002-08-27 14:03:02 +0200

    Nova funkce Nova funkce template <typename Res, typename Seq> join_seq (const Res&, const Seq&). Provadi cinnost stejnou jako stejnojmenna funkce Perlu.
    Nova funkce static std::string printNFA (const NFA_conv&) pro vystup
    zkonvertovane podoby automatu.

--- nfa.hxx
+++ nfa.hxx
@@ -31,8 +31,27 @@ struct NFA_conv {
 typedef std::pair<StateT, StateDeltaT > StateAndDeltaPairT;
 typedef std::pair<LetterT, SetOfStatesT > LetterAndSetOfStatesPairT;
 
-
 /* Funkce pro vystup do streamu */
+template <typename Res, typename Seq>
+static Res join_seq (const Res& sep, const Seq& seq)
+{
+    if (seq.empty())
+	return Res();
+
+    std::basic_ostringstream<typename Res::value_type > ss;
+    typename Seq::const_iterator it = seq.begin();
+    typename Seq::const_iterator next = it;
+    ++next;
+    for (; 
+	 next != seq.end();
+	 ++it, ++next) {
+	ss << *it << sep;
+    }
+    ss << *it;
+
+    return ss.str();
+}
+
 static std::string printNFA (const NFA& nfa)
 {
     std::ostringstream ss;
@@ -58,6 +77,35 @@ static std::string printNFA (const NFA& nfa)
 	ss << "}" << std::endl;
     }
 
+    return ss.str();
+}
+
+static std::string printNFA (const NFA_conv& nfa)
+{
+    std::ostringstream ss;
+    
+    ss << "name " << nfa.name << std::endl;
+    for (std::map<SetOfStatesT, StateDeltaT >::const_iterator dmi 
+	     = nfa.delta.begin();
+	 dmi != nfa.delta.end();
+	 ++dmi) {
+	const SetOfStatesT& st = dmi->first;
+	if (st == nfa.initial)
+	    ss << "initial ";
+	if (nfa.final.find(st) != nfa.final.end())
+	    ss << "final ";
+	ss << "state " << join_seq(std::string("-"),st) << " {" << std::endl;
+	for (StateDeltaT::const_iterator sdi = dmi->second.begin();
+	     sdi != dmi->second.end();
+	     ++sdi) {
+	    ss << sdi->first << " -> { ";
+	    std::copy(sdi->second.begin(), sdi->second.end(),
+		      std::ostream_iterator<StateT >(ss," "));
+	    ss << "}" << std::endl;
+	}
+	ss << "}" << std::endl;
+    }
+    
     return ss.str();
 }
 
