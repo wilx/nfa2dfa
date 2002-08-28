294282405a96045137cdcfccba8cbb64579fb7d3
wilx <none>
2002-08-28 17:29:34 +0200

    Osetri pripadne vyjimky. Osetri pripadne vyjimky. Vypis take zkonvetovany opraveny automat.

--- nfa2dfa.cc
+++ nfa2dfa.cc
@@ -24,6 +24,7 @@ int main(int argc, char* argv[])
 	yyin = stdin;
  
     NFA nfa;
+    NFA_conv nfa_conv;
     int ret = yyparse((void*)&nfa);
     if (ret == 1) {
 	std::cerr << "chyba pri parsovani vstupu" << std::endl;
@@ -32,10 +33,20 @@ int main(int argc, char* argv[])
     /* Ladici vypis vstupniho automatu. */
     std::cout << "input:" << std::endl << printNFA(nfa) << std::endl;
 
-    NFA_conv nfa_conv = convert_NFA2DFA(nfa);
+    try { 
+	nfa_conv = convert_NFA2DFA(nfa);
+    }
+    catch (state_not_found& e) {
+	std::cerr << "chyba pri prevodu v convert_NFA2DFA(): " 
+		  << e.what() << std::endl;
+	std::cerr << "zobrazeni delta na neznamy stav" << std::endl;
+	exit(EXIT_FAILURE);
+    }
 
     /* Ladici vypis prevedeneho automatu */
     std::cout << "converted:" << std::endl << printNFA(nfa_conv) << std::endl;
+    std::cout << "converted fixed:" << std::endl 
+	      << printNFA(fix_converted(nfa_conv,true)) << std::endl;
 
     exit(EXIT_SUCCESS);
 }
