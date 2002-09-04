3c656f972025877dc4dc43de041ec5e2dbd8c397
wilx <none>
2002-09-04 17:04:12 +0200

    Header that provides missing templates.

---
+++ missing.hxx
@@ -0,0 +1,22 @@
+#ifndef MISSING_HXX
+#define MISSING_HXX
+
+#include "config.h"
+
+#ifdef HAVE_STD_SELETCT1ST
+using std::select1st;
+#elif HAVE_GNU_EXT_SELECT1ST
+# include <ext/functional>
+using __gnu_cxx::select1st;
+#else
+/* Copied from SGI's STL */
+// select1st and select2nd are extensions: they are not part of the standard.
+template <class _Pair>
+struct select1st : public unary_function<_Pair, typename _Pair::first_type> {
+  const typename _Pair::first_type& operator()(const _Pair& __x) const {
+    return __x.first;
+  }
+};
+#endif
+
+#endif // MISSING_HXX
